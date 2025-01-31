98c5fd7c65cca4441806077b45574acf838f3bc9
Grant Henderson <grantsirian@gmail.com>
2025-01-31 10:06:40 +0200

    added gitignore. UI tweaks, and responsive touchups on contact

--- obj/Debug/net9.0/BlazorWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d61da82b7998efae535b09446adc1427c5c43a82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30ffca85c3ec547f4014bc1d1196a75aa1b918f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
